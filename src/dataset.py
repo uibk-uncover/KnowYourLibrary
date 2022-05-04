46e34c15d28447f9873ffe55865e1ad3f703814c
nora <nora.hofer@uibk.ac.at>
2022-05-04 17:58:57 +0200

    changes in dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,54 +1,73 @@
 
 import cv2
-import logging
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 from pathlib import Path
 import random
-from typing import Literal, Tuple, List, Union
+from typing import Tuple, List
 
-def load_alaska(size: int = 1000, path: Union[Path,str] = Path.home() / 'Datasets') -> List[str]:
-    """"""
-    # get alaska
-    alaska_path = Path(path) / 'ALASKA_v2_TIFF_256_COLOR'
-    alaska_names = [alaska_path / f for f in os.listdir(alaska_path)]
-    logging.info(f"found ALASKA2 database with {len(alaska_names)} images")
-    # subset
+
+def get_dataset(db_path: Path, sample_size: int) -> List[str]:
+    # TODO-optimization: generate random sample_size numbers and load at index
+    db_names = [db_path / file for file in os.listdir(db_path)]
+    print('Loaded', len(db_names), 'images from', str(db_path))
     random.seed(13245)
-    return [str(p) for p in random.sample(alaska_names, size)]
+    return random.sample(db_names, sample_size)
 
-def load_boss(size: int = 1000, path: Union[Path,str] = Path.home() / 'Datasets') -> List[str]:
-    """"""
-    # get bossbase
-    boss_path = Path(path) / 'BOSS_raw' / 'BOSS_from_raw' # / 'cover'
-    boss_names = [boss_path / f for f in os.listdir(boss_path)]
-    logging.info(f"found BOSS database with {len(boss_names)} images")
-    # subset
-    random.seed(13245)
-    return [str(p) for p in random.sample(boss_names, size)]
 
-# def get_extreme_saturated_samples(paths: List[Union[Path,str]]) -> Tuple[Tuple[Path,int],Tuple[Path,int]]:
-#     """"""
-#     most_saturated, least_saturated = (None, 0), (None, 0)
+def load_alaska_with_extrems(db_path: Path, sample_size: int, img_dimensions: tuple(int, int)) -> np.array:
 
-#     for i, f in enumerate(db_names):
-#         if i % 500 == 0:
-#             mname = (i + '/' + len(db_names) + ' ')
-#             print(i, '/', len(db_names), '         ', end='\r')
-#         if str(f).split('.')[-1] != 'tif':
-#             continue
-#         x = plt.imread(str(f))
-#         xmin, xmax = (x == 0).sum(), (x == 255).sum()
-#         if xmin > least_saturated[1]:
-#             least_saturated = (f, xmin)
-#         if xmax > most_saturated[1]:
-#             most_saturated = (f, xmax)
-#     most_saturated = (db_names / '10343.tif', 98491)
-#     least_saturated = (db_names / '05887.tif', 78128)
-#     return most_saturated,least_saturated
+    db_names = get_dataset(db_path, sample_size)
+    checkerboard = get_checkerboards(img_dimensions, 3)
+    # TODO: write function
+    most_saturated = (db_path / '10343.tif', 98491)
+    least_saturated = (db_path / '05887.tif', 78128)
 
-def get_checkerboard(boardsize: Tuple[int,int], tilesize: Tuple[int,int], channels: int = 3) -> np.ndarray:
+    db = np.array([
+        plt.imread(file)
+        for file in [*db_names, most_saturated[0], least_saturated[0]]
+    ])
+
+    return np.concatenate([db, checkerboard], axis=0)
+
+
+def load_boss_with_extrems(db_path: Path, sample_size: int, img_dimensions: tuple(int, int)) -> np.array:
+
+    db_names = get_dataset(db_path, sample_size)
+    checkerboard = get_checkerboards(img_dimensions, 1)
+    most_saturated = (db_path / '6900_1_3.png', 262144)
+    least_saturated = (db_path / '6155_1_6.png', 88944)
+
+    db = np.array([
+        cv2.imread(str(file), cv2.IMREAD_GRAYSCALE)
+        for file in [*db_names, most_saturated[0], least_saturated[0]]
+    ])
+    db = np.expand_dims(db, axis=3)
+    return np.concatenate([db, checkerboard], axis=0)
+
+    # def get_extreme_saturated_samples(paths: List[Union[Path,str]]) -> Tuple[Tuple[Path,int],Tuple[Path,int]]:
+    #     """"""
+    #     most_saturated, least_saturated = (None, 0), (None, 0)
+
+    #     for i, f in enumerate(db_names):
+    #         if i % 500 == 0:
+    #             mname = (i + '/' + len(db_names) + ' ')
+    #             print(i, '/', len(db_names), '         ', end='\r')
+    #         if str(f).split('.')[-1] != 'tif':
+    #             continue
+    #         x = plt.imread(str(f))
+    #         xmin, xmax = (x == 0).sum(), (x == 255).sum()
+    #         if xmin > least_saturated[1]:
+    #             least_saturated = (f, xmin)
+    #         if xmax > most_saturated[1]:
+    #             most_saturated = (f, xmax)
+    #     most_saturated = (db_names / '10343.tif', 98491)
+    #     least_saturated = (db_names / '05887.tif', 78128)
+    #     return most_saturated,least_saturated
+
+
+def get_checkerboard(boardsize: Tuple[int, int], tilesize: Tuple[int, int], channels: int = 3) -> np.ndarray:
     """Constructs checkerboard with three channels of dimensions boardsize. Each tile has dimension tilesize."""
     board = np.zeros([*boardsize, channels], dtype=np.uint8)
     for i in range(boardsize[0]):
@@ -57,46 +76,10 @@ def get_checkerboard(boardsize: Tuple[int,int], tilesize: Tuple[int,int], channe
                 board[i, j] = 255
     return board
 
-def get_checkerboards(boardsize: Tuple[int,int], channels: int = 3) -> List[np.uint8]:
+
+def get_checkerboards(boardsize: Tuple[int, int], channels: int = 3) -> List[np.uint8]:
     """"""
     return np.array([
         get_checkerboard(boardsize, tilesize, channels=channels)
-        for tilesize in [(4,4),(7,7),(8,8),(15,15),(16,16)]
+        for tilesize in [(4, 4), (7, 7), (8, 8), (15, 15), (16, 16)]
     ])
-
-def get_color_dataset(size: int = 1000, path: Union[Path,str] = Path.home() / 'Datasets') -> np.ndarray:
-    """Returns color dataset of required size."""
-    assert(size >= 5)
-    # alaska
-    alaska_names = load_alaska(size=size - 7, path=path)
-    alaska_path = path / 'ALASKA_v2_TIFF_256_COLOR'
-    # special samples
-    checkerboard = get_checkerboards((256,256), 3)
-    most_saturated = (alaska_path / '10343.tif', 98491)
-    least_saturated = (alaska_path / '05887.tif', 78128)
-    # load
-    alaska = np.array([
-        plt.imread(f)
-        for f in [*alaska_names, most_saturated[0], least_saturated[0]]
-    ])
-    return np.concatenate([alaska, checkerboard], axis=0)
-    
-
-def get_grayscale_dataset(size: int = 1000, path: Union[Path,str] = Path.home() / 'Datasets') -> np.ndarray:
-    """Returns grayscale dataset of required size."""
-    assert(size >= 5)
-    # alaska
-    boss_names = load_boss(size=size - 7, path=path)
-    boss_path = path / 'BOSS_raw' / 'BOSS_from_raw'
-    # special samples
-    checkerboard = get_checkerboards((512,512), 1)
-    most_saturated = (boss_path / '6900_1_3.png',262144)
-    least_saturated = (boss_path / '6155_1_6.png', 88944)
-    # load
-    boss = np.array([
-        cv2.imread(str(f),cv2.IMREAD_GRAYSCALE)
-        for f in [*boss_names, most_saturated[0], least_saturated[0]]
-    ])
-    boss = np.expand_dims(boss, axis=3)
-    return np.concatenate([boss, checkerboard], axis=0)
-    
