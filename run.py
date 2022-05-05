136beb1baca6ecab5a191ae2c47970cfce316038
nora <nora.hofer@uibk.ac.at>
2022-05-05 11:27:43 +0200

    fix ctx dct and fancy_sampling flag

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 
 from src import TestContext
-from src import compression,decompression
+from src import compression, decompression
 from src import output
 from src import python
 from src.simd import *
@@ -11,65 +11,66 @@ sys.path.append('.')
 
 # sampling factor
 samp_factors = [
-    ((1,1),(1,1),(1,1)), # 4:4:4
-    ((1,2),(1,2),(1,2)),
-    ((2,1),(2,1),(2,1)),
-    
-    ((1,2),(1,1),(1,1)), # 4:4:0
-    ((2,2),(2,1),(2,1)),
-    ((1,4),(1,2),(1,2)),
-    ((1,2),(1,2),(1,1)),   # Cb 4:4:4 Cr 4:4:0
-    ((1,2),(1,1),(1,2)),   # Cb 4:4:0 Cr 4:4:4
-    
-    ((2,1),(1,1),(1,1)), # 4:2:2
-    ((2,2),(1,2),(1,2)),
-    ((2,1),(2,1),(1,1)),   # Cb 4:4:4 Cr 4:2:2
-    ((2,1),(1,1),(2,1)),   # Cb 4:2:2 Cr 4:4:4
-    
-    ((2,2),(1,1),(1,1)), # 4:2:0
-    ((2,2),(2,1),(1,1)),   # Cb 4:4:0 Cr 4:2:0
-    ((2,2),(1,1),(2,1)),   # Cb 4:2:0 Cr 4:4:0
-    ((2,2),(1,2),(1,1)),   # Cb 4:2:2 Cr 4:2:0
-    ((2,2),(1,1),(1,2)),   # Cb 4:2:0 Cr 4:2:2
-    ((2,2),(2,2),(1,1)),   # Cb 4:4:4 Cr 4:2:0
-    ((2,2),(2,2),(2,1)),   # Cb 4:4:4 Cr 4:4:0
-    ((2,2),(2,2),(1,2)),   # Cb 4:4:4 Cr 4:2:2
-    ((2,2),(1,1),(2,2)),   # Cb 4:2:0 Cr 4:4:4
-    ((2,2),(2,1),(2,2)),   # Cb 4:4:0 Cr 4:4:4
-    ((2,2),(1,2),(2,2)),   # Cb 4:2:2 Cr 4:4:4
-    
-    ((4,1),(1,1),(1,1)), # 4:1:1
-    ((4,1),(2,1),(1,1)),   # Cb 4:2:2 Cr 4:1:1
-    ((4,1),(1,1),(2,1)),   # Cb 4:1:1 Cr 4:2:2
-    
-    ((4,2),(1,1),(1,1)), # 4:1:0
-    
-    ((1,4),(1,1),(1,1)), # 1:0.5:0
-    ((1,4),(1,2),(1,1)),
-    
-    ((2,4),(1,1),(1,1)), # 2:0.5:0
-    
-    ((3,1),(1,1),(1,1)), # 3:1:1
-    ((3,1),(3,1),(1,1)),   # Cb 4:4:4 Cr 3:1:1
-    ((3,1),(1,1),(3,1)),   # Cb 3:1:1 Cr 4:4:4
-    ((3,2),(3,1),(1,1)), # 3:3:0
-    ((3,2),(1,2),(1,2)), # 3:1:1
+    ((1, 1), (1, 1), (1, 1)),  # 4:4:4
+    ((1, 2), (1, 2), (1, 2)),
+    ((2, 1), (2, 1), (2, 1)),
+
+    ((1, 2), (1, 1), (1, 1)),  # 4:4:0
+    ((2, 2), (2, 1), (2, 1)),
+    ((1, 4), (1, 2), (1, 2)),
+    ((1, 2), (1, 2), (1, 1)),   # Cb 4:4:4 Cr 4:4:0
+    ((1, 2), (1, 1), (1, 2)),   # Cb 4:4:0 Cr 4:4:4
+
+    ((2, 1), (1, 1), (1, 1)),  # 4:2:2
+    ((2, 2), (1, 2), (1, 2)),
+    ((2, 1), (2, 1), (1, 1)),   # Cb 4:4:4 Cr 4:2:2
+    ((2, 1), (1, 1), (2, 1)),   # Cb 4:2:2 Cr 4:4:4
+
+    ((2, 2), (1, 1), (1, 1)),  # 4:2:0
+    ((2, 2), (2, 1), (1, 1)),   # Cb 4:4:0 Cr 4:2:0
+    ((2, 2), (1, 1), (2, 1)),   # Cb 4:2:0 Cr 4:4:0
+    ((2, 2), (1, 2), (1, 1)),   # Cb 4:2:2 Cr 4:2:0
+    ((2, 2), (1, 1), (1, 2)),   # Cb 4:2:0 Cr 4:2:2
+    ((2, 2), (2, 2), (1, 1)),   # Cb 4:4:4 Cr 4:2:0
+    ((2, 2), (2, 2), (2, 1)),   # Cb 4:4:4 Cr 4:4:0
+    ((2, 2), (2, 2), (1, 2)),   # Cb 4:4:4 Cr 4:2:2
+    ((2, 2), (1, 1), (2, 2)),   # Cb 4:2:0 Cr 4:4:4
+    ((2, 2), (2, 1), (2, 2)),   # Cb 4:4:0 Cr 4:4:4
+    ((2, 2), (1, 2), (2, 2)),   # Cb 4:2:2 Cr 4:4:4
+
+    ((4, 1), (1, 1), (1, 1)),  # 4:1:1
+    ((4, 1), (2, 1), (1, 1)),   # Cb 4:2:2 Cr 4:1:1
+    ((4, 1), (1, 1), (2, 1)),   # Cb 4:1:1 Cr 4:2:2
+
+    ((4, 2), (1, 1), (1, 1)),  # 4:1:0
+
+    ((1, 4), (1, 1), (1, 1)),  # 1:0.5:0
+    ((1, 4), (1, 2), (1, 1)),
+
+    ((2, 4), (1, 1), (1, 1)),  # 2:0.5:0
+
+    ((3, 1), (1, 1), (1, 1)),  # 3:1:1
+    ((3, 1), (3, 1), (1, 1)),   # Cb 4:4:4 Cr 3:1:1
+    ((3, 1), (1, 1), (3, 1)),   # Cb 3:1:1 Cr 4:4:4
+    ((3, 2), (3, 1), (1, 1)),  # 3:3:0
+    ((3, 2), (1, 2), (1, 2)),  # 3:1:1
 ]
 
 implementations = {
-    'PIL':python.PIL_IO,
-    'cv2':python.cv2_IO,
-    'plt':python.plt_IO,
-    '6b':python.libjpeg6b_IO,
-    '8d':python.libjpeg8d_IO,
-    '9d':python.libjpeg9d_IO,
-    '9e':python.libjpeg9e_IO,
-    'turbo':python.libjpegturbo_IO
+    'PIL': python.PIL_IO,
+    'cv2': python.cv2_IO,
+    'plt': python.plt_IO,
+    '6b': python.libjpeg6b_IO,
+    '8d': python.libjpeg8d_IO,
+    '9d': python.libjpeg9d_IO,
+    '9e': python.libjpeg9e_IO,
+    'turbo': python.libjpegturbo_IO
 }
+
 
 def run_compression_tests(dataset: np.ndarray):
     """Runs compression tests
-    
+
     TODO:
         - show not only Cb, but all three channels + spatial for check
     """
@@ -88,11 +89,11 @@ def run_compression_tests(dataset: np.ndarray):
     # res = compression.run_test(dataset, TestContext())
     # compression.print_clusters(res)
     # print()
-    
+
     def run_dct_compression_test(samp_factor, use_fancy_sampling=None):
         for dct_method in ['JDCT_ISLOW', 'JDCT_FLOAT', 'JDCT_IFAST']:
             ctx = TestContext()
-            ctx.dct_method = dct_method
+            ctx.dct_method_compression = dct_method
             ctx.samp_factor = samp_factor
             ctx.use_fancy_sampling = use_fancy_sampling
             print("Method:", dct_method)
@@ -102,36 +103,38 @@ def run_compression_tests(dataset: np.ndarray):
     # DCT method
     print("--- DCT methods ---")
     print("4:4:4 no downsampling")
-    run_dct_compression_test( ((1,1),(1,1),(1,1)) )
+    run_dct_compression_test(((1, 1), (1, 1), (1, 1)))
     if dataset.shape[3] == 3:
-        for use_fancy_sampling,method in zip([True,False],['fancy downsampling','simple_scaling']):
+        for use_fancy_sampling, method in zip([True, False], ['fancy downsampling', 'simple_scaling']):
             print(f"4:2:0 {method}")
-            run_dct_compression_test( ((2,2),(1,1),(1,1)), use_fancy_sampling)
+            run_dct_compression_test(
+                ((2, 2), (1, 1), (1, 1)), use_fancy_sampling)
     print()
 
     # quality
     print("--- Quality ---")
-    for quality in range(25,101):
+    for quality in range(25, 101):
         ctx = TestContext()
         ctx.quality = quality
         res = compression.run_test(dataset, ctx)
         output.add_print_grouped_clusters(res, quality)
     output.end_print_grouped_clusters()
     print()
-    
+
     # sampling factor
     if dataset.shape[3] == 3:
         print("--- Sampling factor ---")
         # fancy vs. simple
-        for use_fancy_sampling,method in zip([True,False],['Fancy downsampling','Simple_scaling']):
+        for use_fancy_sampling, method in zip([True, False], ['Fancy downsampling', 'Simple_scaling']):
             print(method)
             # sampling factores
             for samp_factor in samp_factors:
                 ctx = TestContext()
                 ctx.samp_factor = samp_factor
-                ctx.use_chroma_sampling = use_fancy_sampling
+                ctx.use_fancy_sampling = use_fancy_sampling
                 res = compression.run_test(dataset, ctx)
-                compression.add_print_grouped_clusters(res, samp_factor) # TODO
+                compression.add_print_grouped_clusters(
+                    res, samp_factor)  # TODO
             compression.end_print_grouped_clusters()
         print()
 
@@ -145,19 +148,19 @@ def run_compression_tests(dataset: np.ndarray):
             if dataset.shape[3] == 1:
                 compression.add_print_grouped_clusters(res, offset)
             else:
-                compression.add_print_grouped_clusters(res, offset) # TODO
+                compression.add_print_grouped_clusters(res, offset)  # TODO
         compression.end_print_grouped_clusters()
 
     # margin effects
     print("--- Margin effects ---")
     print("4:4:4 no downsampling")
-    run_margin_compression_test([0,1,2,4,7,8],
-                                ((1,1),(1,1),(1,1)))
+    run_margin_compression_test([0, 1, 2, 4, 7, 8],
+                                ((1, 1), (1, 1), (1, 1)))
     if dataset.shape[3] == 3:
-        for use_fancy_sampling,method in zip([True,False],['fancy downsampling','simple_scaling']):
+        for use_fancy_sampling, method in zip([True, False], ['fancy downsampling', 'simple_scaling']):
             print(f"4:2:0 {method}")
-            run_margin_compression_test([16,15,9,8,7,3,2,1],
-                                        ((2,2),(1,1),(1,1)), use_fancy_sampling)
+            run_margin_compression_test([16, 15, 9, 8, 7, 3, 2, 1],
+                                        ((2, 2), (1, 1), (1, 1)), use_fancy_sampling)
         print()
 
     # Python implementations
@@ -170,6 +173,7 @@ def run_compression_tests(dataset: np.ndarray):
         ctx.compressor = python.io_compressor_grayscale
     res = compression.run_test(dataset, ctx)
     compression.print_clusters(res)
+
 
 if __name__ == "__main__":
 
@@ -184,7 +188,7 @@ if __name__ == "__main__":
 
     # compression tests
     run_compression_tests(alaska)
-    #run_compression_tests(boss)
+    # run_compression_tests(boss)
     # decompression tests
-    #run_decompression_tests(alaska)
-    #run_decompression_tests(boss)
+    # run_decompression_tests(alaska)
+    # run_decompression_tests(boss)
